1548a43e7b50515d559d76338816c262328ef040
Chertopologov <Dillerfeed99@yandex.ru>
2025-01-03 21:04:00 +0300

    pu-pu-pu
    asd

--- script.js
+++ script.js
@@ -8,8 +8,19 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slider-text");
-    let slides = document.getElementsByClassName("slider-image");
+  let i;
+  let slidesText = document.getElementsByClassName("slider-content");
+  let dots = document.getElementsByClassName("slider-dots");
+  if (n > slidesText.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slidesText.length}
+  for (i = 0; i < slidesText.length; i++) {
+    slidesText[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slidesText[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " active";
 }
